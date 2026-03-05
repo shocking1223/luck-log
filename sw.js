715ae3d61c3b98e3b20d615abe3fea5425125d22
shocking1223 <sho.nen.yume.naka@gmail.com>
2026-03-05 17:53:23 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = "shoi-lucklog-cache-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
